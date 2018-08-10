934bd79512ddfd0e2ab4d88f83d1ce75adb9d7bb
Kevin Jiang <kevin51jiang@email.com>
2018-08-09 21:21:51 -0400

    WebPack, Basic React Working

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,24 @@
 module.exports = {
+    mode: "development",
     entry: './src/index.js',
     output: {
-        path: __dirname + '/public',
-        filename: 'bundle.js'
+        //path: path.resolve(__dirname, './public'),
+        publicPath: './public',
+        filename: '../public/bundle.js'
     },
     module: {
-        loaders: [
+        rules: [
+            {
+                test: /\.json$/,
+                use: [
+                    'json-loader'
+                ]
+            },
             {
                 test: /\.js$/,
-                loader: 'babel-loader'
+                use: [
+                    'babel-loader'
+                ]
             }
         ]
     }
