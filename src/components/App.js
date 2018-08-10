cf2fe44a72880c197e0be41444e1f2d17b90fc3e
Kevin Jiang <kevin51jiang@email.com>
2018-08-10 06:31:52 -0400

    Module Trees :D

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Header from './Header';
+
+const App = (props) => {
+    return (
+        <div className="App">
+            <Header message="Naming Contests" />
+            <div>
+                ...
+            </div>
+        </div>
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PropTypes from 'prop-types';
 
-const Header = ({ message }) => {
-    return (
-        <h2 align="center" className="Header text-center">
-            {message}
-        </h2>
-    );
-};
-
-Header.propTypes = {
-    message: PropTypes.string.isRequired
-};
-
-
-const App = (props) => {
-    return (
-        <div className="App">
-            <Header message="Naming Contests" />
-            <div>
-
-            </div>
-        </div>
-    );
-};
-
-
-
+import App from './components/App';
 
 ReactDOM.render(
     <App />,
